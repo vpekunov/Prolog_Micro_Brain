fe14f6f39b002f7c4eb3b03b83c4b89362a38c52
Владимир Пекунов <pekunov@mail.ru>
2026-05-14 20:41:29 +0300

    MacBook porting

--- install.sh
+++ install.sh
@@ -5,4 +5,11 @@ cd ./xpathInduct.dir
 fpc -B -O3 -Mobjfpc -FcUTF-8 -fPIC ./xpathInduct.lpr
 cp ./libxpathInduct.so ../
 cd ..
-g++ -o prolog_micro_brain tinyxml2.cpp elements.cpp prolog_micro_brain.cpp -std=c++17 -fpermissive -fopenmp -O3 -lm -lboost_system -lboost_filesystem -ldl
+g++ -o prolog_micro_brain tinyxml2.cpp elements.cpp prolog_micro_brain.cpp -std=c++11 -O4 -lm -lboost_system -lboost_filesystem -ldl -I/opt/homebrew/include -L/opt/homebrew/lib
+if [ $? -ne 0 ]; then
+   g++ -o prolog_micro_brain tinyxml2.cpp elements.cpp prolog_micro_brain.cpp -std=c++11 -O4 -lm -lboost_filesystem -ldl -I/opt/homebrew/include -L/opt/homebrew/lib
+fi
+g++ -c tinyxml2.cpp elements.cpp prolog_micro_brain.cpp -std=c++11 -O4 -lm -lboost_system -lboost_filesystem -ldl -fPIC -I/opt/homebrew/include -L/opt/homebrew/lib
+if [ $? -ne 0 ]; then
+   g++ -c tinyxml2.cpp elements.cpp prolog_micro_brain.cpp -std=c++11 -O4 -lm -lboost_filesystem -ldl -fPIC -I/opt/homebrew/include -L/opt/homebrew/lib
+fi
