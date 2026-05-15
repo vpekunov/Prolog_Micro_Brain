c6f10e6abd4f0f0b1fe2979b8185cc7cd314b75b
Владимир Пекунов <pekunov@mail.ru>
2026-05-15 10:24:42 +0300

    MacBook porting

--- install.sh
+++ install.sh
@@ -4,15 +4,11 @@ chmod -R +x *
 cd ./xpathInduct.dir
 fpc -B -O3 -Mobjfpc -FcUTF-8 -fPIC ./xpathInduct.lpr
 cp ./libxpathInduct.so ../
+cp ./libxpathInduct.dylib ../libxpathInduct.so
 cd ..
 if [[ "$OSTYPE" == "darwin"* ]]; then
-   g++ -o prolog_micro_brain tinyxml2.cpp elements.cpp prolog_micro_brain.cpp -std=c++17 -O4 -lm -lboost_system -lboost_filesystem -ldl -I/opt/homebrew/include -L/opt/homebrew/lib -fpermissive
-   if [ $? -ne 0 ]; then
-      g++ -o prolog_micro_brain tinyxml2.cpp elements.cpp prolog_micro_brain.cpp -std=c++17 -O4 -lm -lboost_filesystem -ldl -I/opt/homebrew/include -L/opt/homebrew/lib -fpermissive
-   fi
+    echo "Running on a Mac"
+   `ls /opt/homebrew/bin/g++*` -o prolog_micro_brain tinyxml2.cpp elements.cpp prolog_micro_brain.cpp -std=c++17 -O4 -lm -ldl -fopenmp -fpermissive
 else
-   g++ -o prolog_micro_brain tinyxml2.cpp elements.cpp prolog_micro_brain.cpp -std=c++17 -O4 -lm -lboost_system -lboost_filesystem -ldl -fopenmp -fpermissive
-   if [ $? -ne 0 ]; then
-      g++ -o prolog_micro_brain tinyxml2.cpp elements.cpp prolog_micro_brain.cpp -std=c++17 -O4 -lm -lboost_filesystem -ldl -fopenmp -fpermissive
-   fi
+   g++ -o prolog_micro_brain tinyxml2.cpp elements.cpp prolog_micro_brain.cpp -std=c++17 -O4 -lm -ldl -fopenmp -fpermissive
 fi
